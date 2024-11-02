e2e7147a2047ee60c097242690749847444599c6
Henri Delozanne <hnr.delozanne@gmail.com>
2024-11-02 10:19:54 +0100

    Create game logic

--- pages/test.vue
+++ pages/test.vue
@@ -1,3 +1,96 @@
 <template>
-  <h1>test</h1>
+  <div>
+    <h1>test</h1>
+    <button @click="startTest">Start</button>
+    <div class="game-wrapper">
+      <div v-if="gameIsOn" class="stats-wrapper">
+        <p>{{ turn + 1 }} / {{ todaysCards.length }}</p>
+        <p>{{ questionsWellAnswered.length }} good answers / {{ turn  }}</p>
+        <p>{{ questionsBadAnswered.length }} bad answers / {{ turn }}</p>
+      </div>
+      <div v-if="questionIsDisplayed" class="question">
+        <p>{{ question }}</p>
+        <button @click="flip">Flip</button>
+      </div>
+      <div v-if="answerIsDisplayed" class="answer">
+        <p>{{ answer }}</p>
+        <div class="evaluate-wrapper">
+          <button @click="evaluate(true)">Good answer</button>
+          <button @click="evaluate(false)">Bad answer</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { allCards, initializeCards } from '../composables/useGetCards';
+import type { Card } from '../types';
+import { isToday, isBefore, startOfToday } from 'date-fns';
+
+initializeCards();
+
+const todaysCards: Ref<Card[]> = ref([]);
+
+onMounted(() => {
+  todaysCards.value = allCards.filter((card: Card) => {
+    return isToday(card.nextTest) || isBefore(card.nextTest, startOfToday());
+  })
+});
+
+const question: Ref<string> = ref('');
+const answer: Ref<string> = ref('');
+const turn: Ref<number> = ref(0);
+const gameIsOn: Ref<boolean> = ref(false);
+const questionIsDisplayed: Ref<boolean> = ref(false);
+const answerIsDisplayed: Ref<boolean> = ref(false);
+const questionsWellAnswered: Ref<Card[]> = ref([]);
+const questionsBadAnswered: Ref<Card[]> = ref([]);
+
+const currentCard = computed(() => {
+  return todaysCards.value[turn.value]
+});
+
+const launchNewTurn = () => {
+  question.value = currentCard.value.question;
+  answer.value = currentCard.value.answer;
+  questionIsDisplayed.value = true;
+}
+
+const startTest = () => {
+  turn.value = 0;
+  launchNewTurn();
+  questionIsDisplayed.value = true;
+  gameIsOn.value = true;
+}
+
+const flip = () => {
+  questionIsDisplayed.value = false;
+  answerIsDisplayed.value = true;
+}
+
+const resetGame = () => {
+  gameIsOn.value = false;
+  turn.value = 0;
+  questionsWellAnswered.value.splice(0, questionsWellAnswered.value.length);
+  questionsBadAnswered.value.splice(0, questionsBadAnswered.value.length);
+}
+
+const evaluate = (goodAnswer: boolean) => {
+  if (goodAnswer) {
+    questionsWellAnswered.value.push(currentCard.value);
+  }
+  if (!goodAnswer) {
+    questionsBadAnswered.value.push(currentCard.value);
+  }
+  questionIsDisplayed.value = false;
+  answerIsDisplayed.value = false;
+  if (turn.value < todaysCards.value.length - 1) {
+    turn.value += 1;
+    launchNewTurn();
+  } else {
+    window.alert(`total : ${questionsWellAnswered.value.length} / ${todaysCards.value.length}`)
+    resetGame();
+  }
+}
+</script>
