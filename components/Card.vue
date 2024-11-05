521fb26953a7c8814c58ce671c7ca627fc2baa77
Henri Delozanne <hnr.delozanne@gmail.com>
2024-11-05 22:17:16 +0100

    Add feature to edit existing card

--- components/Card.vue
+++ components/Card.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="card">
     <div class="card-main">
-      <p>{{ props.card.question }}</p>
-      <p>{{ props.card.answer }}</p>
+      <input v-if="isVisibleInput.question" type="text" v-model="inputs.question" @keyup.enter="confirm('question')">
+      <p v-else @click="editCard('question')">{{ props.card.question }}</p>
+      <input v-if="isVisibleInput.answer" type="text" v-model="inputs.answer" @keyup.enter="confirm('answer')">
+      <p v-else @click="editCard('answer')">{{ props.card.answer }}</p>
     </div>
     <p>Category: {{ props.card.category }}</p>
     <p>Compartment: {{ props.card.compartment }}</p>
@@ -11,7 +13,7 @@
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "vue";
+import { defineProps, defineEmits } from "vue";
 import type { Card } from '../types';
 import { format } from 'date-fns';
 
@@ -19,11 +21,34 @@ const props = defineProps<{
   card: Card;
 }>();
 
+const emit = defineEmits(['updateField']);
+
 const displayedDate = computed(() => {
   if (props.card.nextTest) {
     return format(props.card.nextTest, 'dd/MM/yyyy');
   } return '-'
 })
+
+const inputs = reactive<{question: string, answer: string}>({
+  question: '',
+  answer: ''
+});
+
+const isVisibleInput = reactive<{question: boolean, answer: boolean}>({
+  question: false,
+  answer: false,
+});
+
+const editCard = (type: 'question' | 'answer') => {
+  isVisibleInput[type] = true;
+  inputs[type] = props.card[type];
+}
+
+const confirm = (type: 'question' | 'answer') => {
+  isVisibleInput[type] = false;
+  emit('updateField', { card: props.card, type, value: inputs[type]});
+}
+
 </script>
 
 <style lang="scss">
@@ -37,6 +62,10 @@ const displayedDate = computed(() => {
     text-align: center;
     font-size: 2rem;
     border-bottom: 1px solid black;
+
+    p {
+      cursor: pointer;
+    }
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,25 @@
   <div>
     <h1>Cards</h1>
     <div class="cards-wrapper">
-      <Card v-for="card in allCards" :key="card.id" :card="card" />
+      <Card
+        v-for="card in allCards"
+        :key="card.id"
+        :card="card"
+        @update-field="updateField"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { allCards, initializeCards } from '../composables/useGetCards';
+import type { Card } from '../types';
 
 initializeCards();
+
+const updateField = (payload: { card: Card, type: 'question' | 'answer', value: string}) => {
+  updateLocalStorage({...payload.card, [payload.type]: payload.value})
+}
 
 </script>
 
